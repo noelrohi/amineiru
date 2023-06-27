3a07177dd0f02d0c2d463c98e282cfdc37a6ba7c
gneiru <noelrohi59@gmail.com>
2023-06-27 16:23:18 +0800

    add config and hooks

---
+++ config/site.ts
@@ -0,0 +1,18 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Dramx",
+  description:
+    "An Anime Streaming App built using the new router, server components and everything new in Next.js 13.",
+  url: "https://dramx.rohi.dev",
+  links: {
+    twitter: "https://twitter.com/gneiru",
+    github: "https://github.com/gneiru/dramx",
+  },
+  mainNav: [
+    {
+      title: "Home",
+      href: "/home",
+    },
+  ],
+};
