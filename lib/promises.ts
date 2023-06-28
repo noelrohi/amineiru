43ab30004c7e1d0f7dcde54db3aeba9ad2ac17a5
gneiru <noelrohi59@gmail.com>
2023-06-28 19:51:02 +0800

    isr some promise

--- lib/promises.ts
+++ lib/promises.ts
@@ -1,5 +1,11 @@
 import { absoluteUrl } from "@/lib/utils";
-import { AiringAnime, EpisodeLinks, Info, RecentRelease, SearchedAnime } from "@/types";
+import {
+  AiringAnime,
+  EpisodeLinks,
+  Info,
+  RecentRelease,
+  SearchedAnime,
+} from "@/types";
 
 export const animePromise = {
   topAiring: async (): Promise<AiringAnime> => {
@@ -11,7 +17,9 @@ export const animePromise = {
     return fetch(absoluteUrl("/api/anime/recent")).then((res) => res.json());
   },
   animeInfo: async (id: string): Promise<Info> => {
-    return fetch(absoluteUrl(`/api/anime/${id}`)).then((res) => res.json());
+    return fetch(absoluteUrl(`/api/anime/${id}`), {
+      next: { revalidate: 60 },
+    }).then((res) => res.json());
   },
   episode: async (id: string): Promise<EpisodeLinks> => {
     return fetch(absoluteUrl(`/api/anime/episode/${id}`)).then((res) =>
@@ -19,8 +27,8 @@ export const animePromise = {
     );
   },
   search: async (query: string): Promise<SearchedAnime> => {
-    return fetch(absoluteUrl(`/api/anime/search?query=${query}`)).then((res) =>
-      res.json()
-    );
+    return fetch(absoluteUrl(`/api/anime/search?query=${query}`), {
+      next: { revalidate: 60 },
+    }).then((res) => res.json());
   },
 };
