48fcff5c018fcf14e20336a3a940a0d397f9f110
gneiru <noelrohi59@gmail.com>
2023-06-30 13:05:07 +0800

    update description

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   name: "Dramx",
   description:
-    "An Anime Streaming App built using the new router, server components and everything new in Next.js 13.",
+    "Dramx is a streaming app that lets you watch anime online. It's built using Next.js 13, server components and everything new in the new router.",
   url: "https://dramx.rohi.dev",
   links: {
     twitter: "https://twitter.com/gneiru",
